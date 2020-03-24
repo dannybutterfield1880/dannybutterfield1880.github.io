ab80c98cbe48dabd81cac2573f4dbc863e0838f9
Danny Butterfield <dannybutterfield95@gmail.com>
2020-03-24 10:03:57 +0000

    Updates

---
+++ precache-manifest.1984841d47025e769f8c0af74018c141.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436688fbea030f2e8eba5a0b1e37fc0b",
+    "url": "/dannybutterfield1880.github.io/index.html"
+  },
+  {
+    "revision": "5ea442f53a8eaaa23597",
+    "url": "/dannybutterfield1880.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3a51ed187374089e7d0",
+    "url": "/dannybutterfield1880.github.io/static/js/2.9e384841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dannybutterfield1880.github.io/static/js/2.9e384841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea442f53a8eaaa23597",
+    "url": "/dannybutterfield1880.github.io/static/js/main.759faf3e.chunk.js"
+  },
+  {
+    "revision": "0eb7cb4e0b3ad2449867",
+    "url": "/dannybutterfield1880.github.io/static/js/runtime-main.6399e903.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dannybutterfield1880.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
