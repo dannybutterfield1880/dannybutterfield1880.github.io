c9fde89ab50dde61b4d91ab8855519a3a9e67553
Danny Butterfield <dannybutterfield95@gmail.com>
2020-03-24 10:17:46 +0000

    Updates

---
+++ precache-manifest.59f488b53a354a989b5d802348cb2e53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670674d5c2caadf7634bbcf98df7d41a",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "fc207d82e5846b72ab22",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3a51ed187374089e7d0",
+    "url": "/test/static/js/2.9e384841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.9e384841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc207d82e5846b72ab22",
+    "url": "/test/static/js/main.a7a28a6f.chunk.js"
+  },
+  {
+    "revision": "852230d7b5e268af239b",
+    "url": "/test/static/js/runtime-main.9908fd13.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
